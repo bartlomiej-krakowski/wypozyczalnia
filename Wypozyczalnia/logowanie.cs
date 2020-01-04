f71d513c7de35501f45f9fd9194378c54313c25f
wojtek <wojtekkawlewski@gmail.com>
2020-01-04 15:18:28 +0100

    klasa dodana

---
+++ Wypozyczalnia/logowanie.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wypozyczalnia_logowanie
+{
+    class logowanie
+    {
+        private int login;
+        private int password;
+
+        public logowanie(int login, int password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+
+        public int Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    login = value;
+                }
+                else
+                {
+                    Console.WriteLine("Bład podano pusty ciąg znaków");
+                }
+
+            }
+        }
+
+        public int Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Bład podano pusty ciąg znaków");
+                }
+            }
+        }
+
+        public void log()
+        {
+            Console.WriteLine("Podaj login: ");
+
+            Console.WriteLine("Podaj hasło: ");
+            
+
+
+        }
+    }
+}
