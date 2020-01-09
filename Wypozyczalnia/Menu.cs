1ba60133c53b2257c5be309fab0c8c36f758454d
wojtek230689 <wojtekkawlewski@gmail.com>
2020-01-09 19:33:34 +0100

    klasa dodana zmiana menu

---
+++ Wypozyczalnia/Menu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+
+namespace Wypozyczalnia
+{
+    public class Menu
+    {
+        enum Admin { dodaj_samochód = 1, usuń_samochód, lista_aut, dodaj_klienta, usuń_klienta, lista_pracowników, dodaj_wypożyczenie, anuluj_wypożyczenie, zmodyfikuj_wypozyczenie, dodaj_pracownika, usuń_pracownika, licencja_programu, statystyka, wyloguj };
+        enum User { dodaj_klienta = 1, dodaj_samochód, lista_aut, dodaj_wypozyczenie, anuluj_wypozyczenie, zmodyfikuj_wypożyczenie, wyloguj };
+
+        public void menu()
+        {
+            int i = 1;
+
+            foreach (Admin admin in (Admin[])Enum.GetValues(typeof(Admin)))
+            {
+                Console.Write($"[{i++}]. ");
+                Console.WriteLine(String.Concat(admin.ToString().Replace('_', ' ')));
+            }
+
+            bool exit = true;
+
+            Admin start;
+            string choosenOption = Console.ReadLine().Replace(' ', '_');
+
+
+            bool AdminConfirmed = Enum.TryParse<Admin>(choosenOption, out start);
+
+            if (!AdminConfirmed)
+            {
+                Console.WriteLine("Wybrałeś niepoprawną opcję");
+            }
+
+            switch (start)
+            {
+                case Admin.dodaj_samochód:
+                    break;
+                case Admin.usuń_samochód:
+                    break;
+                case Admin.lista_aut:
+                    break;
+                case Admin.dodaj_klienta:
+                    break;
+                case Admin.usuń_klienta:
+                    break;
+                case Admin.lista_pracowników:
+                    break;
+                case Admin.dodaj_wypożyczenie:
+                    break;
+                case Admin.anuluj_wypożyczenie:
+                    break;
+                case Admin.zmodyfikuj_wypozyczenie:
+                    break;
+                case Admin.dodaj_pracownika:
+                    break;
+                case Admin.usuń_pracownika:
+                    break;
+                case Admin.licencja_programu:
+                    break;
+                case Admin.statystyka:
+                    break;
+                case Admin.wyloguj:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Wypozyczalnia/logowanie.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wypozyczalnia_logowanie
+{
+    class logowanie
+    {
+        private string login;
+        private SecureString securePwd;
+
+        public logowanie()
+        {
+           
+            
+        }
+
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    login = value;
+                }
+                else
+                {
+                    Console.WriteLine("Bład podano pusty ciąg znaków");
+                }
+
+            }
+        }
+
+
+        public void log()
+        {
+            Console.WriteLine("Podaj login: ");
+            Login = Console.ReadLine();
+            
+
+            Console.WriteLine("Podaj hasło: ");
+            
+            securePwd = new SecureString();
+            ConsoleKeyInfo key;
+
+            Console.Write("Enter password: ");
+            do {
+                key = Console.ReadKey(true);
+           
+                // Ignore any key out of range.
+                if (((int) key.Key) >= 65 && ((int) key.Key <= 90)) {
+                    // Append the character to the password.
+                    securePwd.AppendChar(key.KeyChar);
+                    Console.Write("*");
+                }   
+                // Exit if Enter key is pressed.
+            } while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+
+            try
+            {
+                Process.Start("Notepad.exe", "MyUser", securePwd, "MYDOMAIN");
+            }
+            catch (Win32Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                securePwd.Dispose();
+            }
+
+            
+
+        }
+    }
+}
